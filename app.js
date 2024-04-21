0ea2b7f327a5571da54afdc4f9062a234ca6e7c3
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-04-21 20:48:56 +0100

    Connect app to database

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// application
+const app = express();
+
+const connectionUrl = 'mongodb://localhost:27017/todoDb';
+
+mongoose.connect(connectionUrl)
+    .then(() => {
+        console.log("connection to database successful")
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
 
 const PORT = 3000;
 
 //templating engine
 app.set('view engine', 'ejs');
 
-// application
-const app = express();
 
 //listening to server
 app.listen(PORT, () => {
