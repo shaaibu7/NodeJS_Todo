6e449888e89590458d1c511e7f2907c2b0c7b183
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-04-22 10:03:33 +0100

    Implement functionality for adding new todo

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { type } = require('os');
 const path = require('path')
+const bodyParser = require('body-parser')
 
 // application
 const app = express();
@@ -15,11 +17,16 @@ mongoose.connect(connectionUrl)
         console.log(error.message)
     })
 
+    const todoSchema = mongoose.Schema({title: { type: String, required: true }, desc: String}, { timestamps: true })
+
+    const Todo = mongoose.model('todo', todoSchema)
 const PORT = 8000;
 
 //templating engine
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res, next) => {
     try {
@@ -50,6 +57,24 @@ app.get('/delete-todo', (req, res, next) => {
         res.render('deleteTodo', { title: "Delete Todo" })
     } catch (error) {
         res.status(500).json({ message: message.error })
+    }
+})
+
+// add todo
+
+app.post('/add-todo', async (req, res, next) => {
+    try {
+        const { title, desc } = req.body;
+
+        if(!title) {
+            return res.status(400).json({ message: "Title is required" });
+        }
+
+        const newTodo = new Todo({ title, desc });
+        await newTodo.save();
+        res.redirect('/');
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 })
 
